128a3db7302f7918ea6fadbde676a0544ef3c4bd
Bernardngari <bernardngari.ke@gmail.com>
2022-06-14 00:09:40 +0300

    Add the 'add item' functionality to the to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector( "form" );
+  let inputField = form.querySelector( "#new-task-description" );
+  let ul = document.querySelector( '#tasks' );
+  let submitBtn = form.querySelector( '#submit' );
+  
+  form.addEventListener('submit', (event) =>{
+    //console.log(event); 
+    event.preventDefault();
+    let li = document.createElement( 'li' );
+    ul.appendChild( li );
+    li.textContent = inputField.value;
+    form.reset();
+
+  })
+  
+
 });
